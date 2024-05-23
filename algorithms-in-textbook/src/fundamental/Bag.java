694d3e5e948c8503c4cfac7fb03c99357e0f3498
Annisa Nabila Az-Zahra <annisana1204@gmail.com>
2024-05-23 18:52:51 +0700

    feat: Add linked-list based bag

---
+++ algorithms-in-textbook/src/fundamental/Bag.java
@@ -0,0 +1,87 @@
+package fundamental;
+
+import edu.princeton.cs.algs4.StdIn;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/******************************************************************************
+ *  Original Author: Robert Sedgewick and Kevin Wayne
+ *  Data: tobe.txt
+ ******************************************************************************/
+
+public class Bag<E> implements Iterable<E> {
+    private Node<E> first; // Least recently added item
+    private int n; // Bag size
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+    }
+
+    public Bag() {
+        first = null;
+        n = 0;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void add(E item) {
+        Node<E> oldFirst = first;
+        first = new Node<>();
+        first.item = item;
+        first.next = oldFirst;
+        n++;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new LinkedIterator(first);
+    }
+
+    private class LinkedIterator implements Iterator<E> {
+        private Node<E> current;
+
+        public LinkedIterator(Node<E> first) {
+            current = first;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            E item = current.item;
+            current = current.next;
+
+            return item;
+        }
+    }
+
+    public static void main(String[] args) {
+        Bag<String> bag = new Bag<>();
+
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            bag.add(item);
+        }
+
+        System.out.println("Bag size : " + bag.size());
+
+        for (String item : bag) {
+            System.out.println(item);
+        }
+    }
+}
