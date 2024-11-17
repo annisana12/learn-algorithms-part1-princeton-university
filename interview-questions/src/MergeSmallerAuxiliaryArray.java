873d1faa86f9f9eb8bfeba36662d3cc50bf98be0
Annisa Nabila Az-Zahra <annisana1204@gmail.com>
2024-11-17 12:06:33 +0700

    add merge algorithms in merge sort with smaller auxiliary array

---
+++ interview-questions/src/MergeSmallerAuxiliaryArray.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+/**
+ * Question :
+ * Suppose that the subarray a[0] and a[n-1] is sorted and
+ * the subarray a[n] to a[2*n - 1] is sorted. How can you merge
+ * the two subarrays so that a[0] to a[2*n - 1] is sorted using
+ * an auxiliary array of length n (instead of 2n)
+ * <p>
+ * Note :
+ * Solution for ascending order
+ */
+
+public class MergeSmallerAuxiliaryArray {
+    private static <T extends Comparable<T>> boolean less(T a, T b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static <T extends Comparable<T>> boolean isSorted(T[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++) {
+            if (less(a[i], a[i - 1])) return false;
+        }
+
+        return true;
+    }
+
+    public static <T extends Comparable<T>> void merge(T[] a, T[] aux, int lo, int mid, int hi) {
+        for (int k = lo; k <= mid; k++) {
+            aux[k] = a[k];
+        }
+
+        int i = lo;
+        int j = mid + 1;
+
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) a[k] = a[j++];
+            else if (j > hi) a[k] = aux[i++];
+            else if (less(a[j], aux[i])) a[k] = a[j++];
+            else a[k] = aux[i++];
+        }
+
+        assert isSorted(a, lo, hi);
+    }
+
+    public static void main(String[] args) {
+        String[] a = {"E", "E", "G", "M", "O", "R", "R", "S", "A", "E", "E", "L", "M", "P", "T", "X"};
+
+        int n = a.length;
+        int lo = 0;
+        int hi = n - 1;
+        int mid = lo + (hi - lo) / 2;
+
+        String[] aux = new String[n];
+
+        merge(a, aux, lo, mid, hi);
+
+        System.out.println(Arrays.toString(a));
+    }
+}
